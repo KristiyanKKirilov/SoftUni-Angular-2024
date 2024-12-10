68e1fde55c967a8d4e917832e1dc15972874755a
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-12-10 19:11:44 +0200

    all-cars view extended, add new car component created, html structure nearly finished and css is completed

--- vehicle-app/src/app/app.routes.ts
+++ vehicle-app/src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { BrandsComponent } from './brands/brands.component';
-import { AllCarsComponent } from './all-cars/all-cars.component';
+import { AllCarsComponent } from './car/all-cars/all-cars.component';
 import { LoginComponent } from './user/login/login.component';
 import { AuthenticationComponent } from './user/authentication/authentication.component';
+import { AddCarComponent } from './car/add-car/add-car.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'brands', component: BrandsComponent},
     {path: 'all-cars', component: AllCarsComponent},
-    {path: 'auth', component: AuthenticationComponent}
+    {path: 'auth', component: AuthenticationComponent},
+    {path: 'add-car', component: AddCarComponent},
 
 ];
