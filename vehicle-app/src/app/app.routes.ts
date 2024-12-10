43ea619179f2a1b9bdaa01ee80c271c2f925b2ab
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-12-10 14:43:46 +0200

    all-cars example view created

--- vehicle-app/src/app/app.routes.ts
+++ vehicle-app/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { BrandsComponent } from './brands/brands.component';
+import { AllCarsComponent } from './all-cars/all-cars.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
-    {path: 'brands', component: BrandsComponent}
+    {path: 'brands', component: BrandsComponent},
+    {path: 'all-cars', component: AllCarsComponent},
+    
 ];
