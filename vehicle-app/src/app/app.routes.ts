0b8eec1d7d62efb3d4181b0b6e58ea0ca56e23d6
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-12-10 20:53:25 +0200

    car details view created

--- vehicle-app/src/app/app.routes.ts
+++ vehicle-app/src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { BrandsComponent } from './brands/brands.component';
-import { AllCarsComponent } from './car/all-cars/all-cars.component';
-import { LoginComponent } from './user/login/login.component';
 import { AuthenticationComponent } from './user/authentication/authentication.component';
-import { AddCarComponent } from './car/add-car/add-car.component';
 import { CarDetailsComponent } from './car/car-details/car-details.component';
 import { ErrorComponent } from './error/error.component';
+import { AllCarsComponent } from './car/all-cars/all-cars.component';
+import { AddCarComponent } from './car/add-car/add-car.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
