a09036bfd4c36594ee8ba95c60a398eeca79a826
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-12-10 19:59:14 +0200

    error component created, view implemented and routes configured

--- vehicle-app/src/app/app.routes.ts
+++ vehicle-app/src/app/app.routes.ts
@@ -5,13 +5,20 @@ import { AllCarsComponent } from './car/all-cars/all-cars.component';
 import { LoginComponent } from './user/login/login.component';
 import { AuthenticationComponent } from './user/authentication/authentication.component';
 import { AddCarComponent } from './car/add-car/add-car.component';
+import { CarDetailsComponent } from './car/car-details/car-details.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'brands', component: BrandsComponent},
-    {path: 'all-cars', component: AllCarsComponent},
+    {path: 'cars', children: [
+        {path: '', component: AllCarsComponent},
+        {path: ':carId', component: CarDetailsComponent}
+    ]},
     {path: 'auth', component: AuthenticationComponent},
     {path: 'add-car', component: AddCarComponent},
+    {path: '404', component: ErrorComponent},
+    {path: '**', redirectTo: '/404'}
 
 ];
