7cb7f5e449353309ba4d61139484d3d05e800b55
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-12-19 18:27:09 +0200

    home page css fixed

--- vehicle-app/src/app/home/home.component.ts
+++ vehicle-app/src/app/home/home.component.ts
@@ -2,27 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { LoaderComponent } from '../shared/loader/loader.component';
 import { CarService } from '../car/car.service';
 import { Car } from '../types/car';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LoaderComponent],
+  imports: [
+    LoaderComponent,
+    RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
   cars: Car[] = [];
   isLoading: boolean = true;
 
-  constructor(private carService: CarService){}
+  constructor(private carService: CarService) { }
 
   ngOnInit(): void {
     this.carService.getLatestCars()
-    .subscribe(cars => {
-      this.cars = cars;
-      this.isLoading = false;
-    })  
-    
+      .subscribe(cars => {
+        this.cars = cars;
+        this.isLoading = false;
+      })
+
   }
 
 }
