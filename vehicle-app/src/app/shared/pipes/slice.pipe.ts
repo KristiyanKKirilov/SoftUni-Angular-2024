d76e2242a2a911981ce76948b1b6bf144d5e1045
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-12-19 22:39:31 +0200

    slice pipe created

---
+++ vehicle-app/src/app/shared/pipes/slice.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { max } from 'rxjs';
+
+@Pipe({
+  name: 'slice',
+  standalone: true
+})
+export class SlicePipe implements PipeTransform {
+
+  transform(value: string, maxCharCount = 5): unknown {
+    const dots =  value.length > maxCharCount ? '...' : '';
+    return `${value.substring(0, maxCharCount)}${dots}`;
+  }
+
+}
